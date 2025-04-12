65fb7203c4bafa822a91c41e13b2acbc813cac85
void-the-droid <bhuwan626282@gmail.com>
2025-04-12 13:17:49 +0545

    ease

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,6 +1,8 @@
 import Navbar from "../../components/Navigation/Navbar";
 import "./HomePage.css";
 import CursorFollower from "../../components/UI/CustomCursor";
+import { FaStarOfLife } from "react-icons/fa";
+
 
 function HomePage() {
   return (
@@ -22,8 +24,44 @@ function HomePage() {
           <div className="box box4 bg-red-400"></div>
         </div>
       </div>
+      <div className="text-content flex justify-center item-center  p-5 text-4xl w-full">
+      <Description/>
+
+      </div>
     </div>
   );
 }
 
 export default HomePage;
+
+
+export const  Description = ()=>{
+return(
+<div className="text-container">
+<div className="text-content flex  p-5  w-[75vw]">
+<h3 className="p-4 text-center "> Puremod Clothing for Elevated Everyday Life . Styles change
+<span className="inline-flex items-center justify-center  border-2 rounded-full pr-4 pl-4 m-3 w-fit">
+  <FaStarOfLife className="text-2xl h-6 w-6 mr-2" />
+  with seasons
+</span>
+    
+    united by the liberating essence of travel-inspired  lightheart </h3>
+</div>
+
+<ImageBubble ImgUrl="src\assets\images\image-bubble1.jpg" position='top-[102%] left-[35%]' />
+<ImageBubble ImgUrl="src\assets\images\image-bubble2.jpg" position ="top-[130%] left-[58%]"/>
+</div>
+)
+}
+
+export const ImageBubble = ({ImgUrl,position}) => {
+  return (
+    <div className={`bubble-container absolute ${position}  w-30 h-30 bg-amber-300 border-2 border-gray-300  rounded-full`}>
+      <img
+        src={ImgUrl}
+        alt="Bubble"
+        className="bubble-image   w-30 h-30 object-cover object-[50%_35%]  rounded-full"
+      />
+    </div>
+  );
+}
